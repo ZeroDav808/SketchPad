d55ace00125279018680bc3091ce1e15415e9d95
ZeroDav808 <199318346+ZeroDav808@users.noreply.github.com>
2025-03-05 20:08:35 -0600

    Added the ability to change the color of the squares and reset the color back to its origin

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
 const outerContainer = document.querySelector(".main-container");
 
-function createGrid() {
-    const numOfCols = 16;
-    const numOfRows = 16;
+const numOfCols = 16;
+const numOfRows = 16;
 
-    for(let i = 0; i < numOfCols; i++){
+function creategrid() {
+    for (let i = 0; i < numOfCols; i++) {
         const newContainer = document.createElement("div");
         newContainer.classList.add(`inner-container${i}`);
-        for(let j = 0; j < numOfRows; j++){
-            const newDiv = document.createElement("div");
-            newDiv.classList.add(`item${j}`);
-            newDiv.style.border = "2px solid black";
-            newDiv.style.padding = "20px";
-            newDiv.style.backgroundColor = "#eb2acb";
-            newDiv.style.boxSizing = "border-box";
-            newContainer.append(newDiv);
-        }
-        outerContainer.append(newContainer);
+      for (let j = 0; j < numOfRows; j++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add(`item`);
+        newDiv.style.border = "1px solid black";
+        newDiv.style.padding = "20px";
+        newDiv.style.backgroundColor = "#eb2acb";
+        newDiv.style.boxSizing = "border-box";
+        newContainer.append(newDiv);
+      }
+      outerContainer.append(newContainer);
     }
 }
 
-createGrid();
+
+function randomNum(max) {
+  return Math.floor(Math.random() * (max - 0 + 1) + 0);
+}
+
+
+creategrid();
+
+const items = document.querySelectorAll(".item");
+
+items.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = "blue";
+
+        setTimeout(() => {
+            item.style.backgroundColor = "#eb2acb";
+        }, 500);
+    });
+});
